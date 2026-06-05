ebf6bd7ac0ceac6e7cb1a82e9c363177b09db6d6
Anuragkushwah21 <anuragkushwah5100@gmail.com>
2026-06-05 19:00:37 +0530

    fix icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,19 +13,19 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: '/icon-light-32x32.png',
+        url: '/next.svg',
         media: '(prefers-color-scheme: light)',
       },
       {
-        url: '/icon-dark-32x32.png',
+        url: '/next.svg',
         media: '(prefers-color-scheme: dark)',
       },
       {
-        url: '/icon.svg',
+        url: '/next.svg',
         type: 'image/svg+xml',
       },
     ],
-    apple: '/apple-icon.png',
+    apple: '/vercel.svg',
   },
 }
 
